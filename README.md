a6fc3112a89250aa57ef44756e61c150317dc145
Shivam Pranjale <shivamv.pranjale@gmail.com>
2024-04-25 11:57:12 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # NLP_Hate_Speech_Classification_Project
+
+
+## How to Run
+
+'''bash
+conda create -n hate python=3.8 -y
+'''
+
+'''bash
+conda activate hate
+'''
+
+'''bash
+pip install -r requirements.txt
+'''
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author = "Shivam",
+    author_email="shivamv.pranjale@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
